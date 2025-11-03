6fa7c7be74f8b4ff1d22ff2e5202421cb1650f6c
jayeshgawali205 <jayeshgawali205@gmail.com>
2025-11-03 11:40:33 +0530

    responsive code added

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,47 @@ import ImageCard from "./components/ImageCard";
 import ImageSearch from "./components/ImageSearch";
 
 function App() {
-  const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [term, setTerm] = useState("");
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [term, setTerm] = useState("");
 
-  useEffect(() => {
-    fetch(
-      `https://pixabay.com/api/?key=${
-        import.meta.env.VITE_PIXABAY_API_KEY
-      }&q=${term}&image_type=photo&pretty=true`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        setImages(data.hits);
-        setIsLoading(false);
-      })
-      .catch((err) => console.log(err));
-  }, [term]);
+    useEffect(() => {
+        fetch(
+            `https://pixabay.com/api/?key=${
+                import.meta.env.VITE_PIXABAY_API_KEY
+            }&q=${term}&image_type=photo&pretty=true`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setImages(data.hits);
+                setIsLoading(false);
+            })
+            .catch((err) => console.log(err));
+    }, [term]);
 
-  return (
-    <div className="container mx-auto">
-      <ImageSearch searchText={(text) => setTerm(text)} />
+    return (
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+            <ImageSearch searchText={(text) => setTerm(text)} />
 
-        {!isLoading && images.length === 0 && <h1 className="text-3xl text-center mx-auto my-28">No Images found</h1>}
+            {!isLoading && images.length === 0 && (
+                <h1 className="text-2xl sm:text-3xl text-center mx-auto my-16 sm:my-28 px-4">
+                    No Images found
+                </h1>
+            )}
 
-      {isLoading ? (
-        <h1 className="text-6xl text-center mx-auto my-32">Loading...</h1>
-      ) : (
-        <div className="grid grid-cols-3 gap-1">
-          {images.map((image) => (
-            <ImageCard key={image.id} image={image} />
-          ))}
+            {isLoading ? (
+                <h1 className="text-4xl sm:text-5xl lg:text-6xl text-center mx-auto my-20 sm:my-32 px-4">
+                    Loading...
+                </h1>
+            ) : (
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 lg:gap-8">
+                    {images.map((image) => (
+                        <ImageCard key={image.id} image={image} />
+                    ))}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default App;
